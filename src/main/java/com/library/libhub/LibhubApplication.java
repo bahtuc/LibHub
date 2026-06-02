feafcba6d25bd13209b639a1e307fbb5e63a6c93
BachNone <tranbaothinh00@gmail.com>
2026-06-02 12:25:36 +0700

    Initial commit

---
+++ src/main/java/com/library/libhub/LibhubApplication.java
@@ -0,0 +1,13 @@
+package com.library.libhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/libhub/LibhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.libhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

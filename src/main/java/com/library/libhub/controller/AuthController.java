f28d088924a8770500d0ec0df4ef05b266b48060
CindoSaur <gglau223@gmail.com>
2026-06-16 17:21:36 +0700

    create PostMapping change-password

--- src/main/java/com/library/libhub/controller/AuthController.java
+++ src/main/java/com/library/libhub/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.library.libhub.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.library.libhub.DTO.Request.LoginRequest;
 import com.library.libhub.DTO.Request.RegisterRequest;
+import com.library.libhub.DTO.Request.ChangePasswordRequest;
 import com.library.libhub.service.IAuthService;
 
 @RestController
@@ -33,4 +35,12 @@ public class AuthController {
         return ResponseEntity.ok(
                 authService.login(request));
     }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(
+            @RequestBody ChangePasswordRequest request) {
+        return ResponseEntity.ok(
+                authService.changePassword(request));
+    }
+
 }
